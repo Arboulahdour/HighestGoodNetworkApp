858124f67f476032d784a7ace064e9c00c61092b
Nithesh AN <nithuan141@gmail.com>
2021-01-18 00:13:27 +0530

    URL correction

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,54 +1,62 @@
 
-// API end p[oint will be taking from the config json.
-
-import { ApiEndpointURL } from "../components/App";
-
 let APIEndpoint = process.env.REACT_APP_APIENDPOINT;
 
-if (!APIEndpoint) {
-  APIEndpoint = ApiEndpointURL;
+class Enpoints {
+
+  constructor() {
+    let ApiEndpointURL = "http://hgnrestdev.azurewebsites.net/api";
+    fetch('/config.json')
+      .then(response => response.json())
+      .then((data) => {
+        ApiEndpointURL = data.restapi;
+      });
+    if (!APIEndpoint) {
+      APIEndpoint = ApiEndpointURL;
+    }
+  }
+
+  USER_PROFILE = userId => `${APIEndpoint}/userprofile/${userId}`;
+  USER_PROFILES = () => `${APIEndpoint}/userprofile/`;
+  USER_TEAM = userId => `${APIEndpoint}/userprofile/teammembers/${userId}`;
+  LOGIN = () => `${APIEndpoint}/login`;
+  PROJECTS = () => `${APIEndpoint}/projects`;
+  TEAM = () => `${APIEndpoint}/team`;
+  TEAM_DATA = teamId => `${APIEndpoint}/team/${teamId}`;
+  TEAM_USERS = teamId => `${APIEndpoint}/team/${teamId}/users`;
+  USER_PROJECTS = userId => `${APIEndpoint}/projects/user/${userId}`;
+  PROJECT = () => `${APIEndpoint}/project/`;
+  PROJECT_MEMBER = projectId => `${APIEndpoint}/project/${projectId}/users`;
+  UPDATE_PASSWORD = userId => `${APIEndpoint}/userprofile/${userId}/updatePassword`;
+  FORCE_PASSWORD = () => `${APIEndpoint}/forcepassword`;
+  LEADER_BOARD = userId => `${APIEndpoint}/dashboard/leaderboard/${userId}`;
+  TIME_ENTRIES_PERIOD = (userId, fromDate, toDate) =>
+    `${APIEndpoint}/TimeEntry/user/${userId}/${fromDate}/${toDate}`;
+  TIME_ENTRY = () => `${APIEndpoint}/TimeEntry`;
+  TIME_ENTRY_CHANGE = timeEntryId => `${APIEndpoint}/TimeEntry/${timeEntryId}`;
+  WBS_ALL = () => `${APIEndpoint}/wbs`;
+  TIMER = userId => `${APIEndpoint}/timer/${userId}`;
+  WBS = projectId => `${APIEndpoint}/wbs/${projectId}`;
+  TASKS = wbsId => `${APIEndpoint}/tasks/${wbsId}`;
+  TASK = wbsId => `${APIEndpoint}/task/${wbsId}`;
+  TASK_WBS = wbsId => `${APIEndpoint}/task/wbs/del/${wbsId}`;
+  TASKS_UPDATE = () => `${APIEndpoint}/tasks/update`;
+  TASKS_BY_USERID = userId => `${APIEndpoint}/tasks/userProfile/${userId}`;
+  TASK_DEL = taskId => `${APIEndpoint}/task/del/${taskId}`;
+  GET_TASK = taskId => `${APIEndpoint}/task/${taskId}`;
+  TASK_UPDATE = taskId => `${APIEndpoint}/task/update/${taskId}`;
+  GET_USER_BY_NAME = name => `${APIEndpoint}/userprofile/name/${name}`;
+  FIX_TASKS = wbsId => `${APIEndpoint}/tasks/${wbsId}`;
+  UPDATE_PARENT_TASKS = wbsId => `${APIEndpoint}/task/updateAllParents/${wbsId}`;
+  MOVE_TASKS = wbsId => `${APIEndpoint}/tasks/moveTasks/${wbsId}`;
+  WEEKLY_SUMMARIES_REPORT = () => `${APIEndpoint}/reports/weeklysummaries`;
+  TEAM_MEMBERS = teamId => `${APIEndpoint}/team/${teamId}/users`;
+  TEAM_BY_ID = teamId => `${APIEndpoint}/team/${teamId}`;
+  TASKNOTIFICATION = () => `${APIEndpoint}/tasknotification`;
+  MARK_TASK_NOTIFICATION_READ = taskId => `${APIEndpoint}/tasknotification/read/${taskId}`;
+  USER_UNREAD_TASK_NOTIFICATIONS = userId => `${APIEndpoint}/tasknotification/user/${userId}`;
 }
 
-export const ENDPOINTS = {
-  USER_PROFILE: userId => `${APIEndpoint}/userprofile/${userId}`,
-  USER_PROFILES: () => `${APIEndpoint}/userprofile/`,
-  USER_TEAM: userId => `${APIEndpoint}/userprofile/teammembers/${userId}`,
-  LOGIN: () => `${APIEndpoint}/login`,
-  PROJECTS: () => `${APIEndpoint}/projects`,
-  TEAM: () => `${APIEndpoint}/team`,
-  TEAM_DATA: teamId => `${APIEndpoint}/team/${teamId}`,
-  TEAM_USERS: teamId => `${APIEndpoint}/team/${teamId}/users`,
-  USER_PROJECTS: userId => `${APIEndpoint}/projects/user/${userId}`,
-  PROJECT: () => `${APIEndpoint}/project/`,
-  PROJECT_MEMBER: projectId => `${APIEndpoint}/project/${projectId}/users`,
-  UPDATE_PASSWORD: userId => `${APIEndpoint}/userprofile/${userId}/updatePassword`,
-  FORCE_PASSWORD: () => `${APIEndpoint}/forcepassword`,
-  LEADER_BOARD: userId => `${APIEndpoint}/dashboard/leaderboard/${userId}`,
-  TIME_ENTRIES_PERIOD: (userId, fromDate, toDate) =>
-    `${APIEndpoint}/TimeEntry/user/${userId}/${fromDate}/${toDate}`,
-  TIME_ENTRY: () => `${APIEndpoint}/TimeEntry`,
-  TIME_ENTRY_CHANGE: timeEntryId => `${APIEndpoint}/TimeEntry/${timeEntryId}`,
-  WBS_ALL: () => `${APIEndpoint}/wbs`,
-  TIMER: userId => `${APIEndpoint}/timer/${userId}`,
-  WBS: projectId => `${APIEndpoint}/wbs/${projectId}`,
-  TASKS: wbsId => `${APIEndpoint}/tasks/${wbsId}`,
-  TASK: wbsId => `${APIEndpoint}/task/${wbsId}`,
-  TASK_WBS: wbsId => `${APIEndpoint}/task/wbs/del/${wbsId}`,
-  TASKS_UPDATE: () => `${APIEndpoint}/tasks/update`,
-  TASKS_BY_USERID: userId => `${APIEndpoint}/tasks/userProfile/${userId}`,
-  TASK_DEL: taskId => `${APIEndpoint}/task/del/${taskId}`,
-  GET_TASK: taskId => `${APIEndpoint}/task/${taskId}`,
-  TASK_UPDATE: taskId => `${APIEndpoint}/task/update/${taskId}`,
-  GET_USER_BY_NAME: name => `${APIEndpoint}/userprofile/name/${name}`,
-  FIX_TASKS: wbsId => `${APIEndpoint}/tasks/${wbsId}`,
-  UPDATE_PARENT_TASKS: wbsId => `${APIEndpoint}/task/updateAllParents/${wbsId}`,
-  MOVE_TASKS: wbsId => `${APIEndpoint}/tasks/moveTasks/${wbsId}`,
-  WEEKLY_SUMMARIES_REPORT: () => `${APIEndpoint}/reports/weeklysummaries`,
-  TEAM_MEMBERS: teamId => `${APIEndpoint}/team/${teamId}/users`,
-  TEAM_BY_ID: teamId => `${APIEndpoint}/team/${teamId}`,
-  TASKNOTIFICATION: () => `${APIEndpoint}/tasknotification`,
-  MARK_TASK_NOTIFICATION_READ: taskId => `${APIEndpoint}/tasknotification/read/${taskId}`,
-  USER_UNREAD_TASK_NOTIFICATIONS: userId => `${APIEndpoint}/tasknotification/user/${userId}`,
-};
+const ENDPOINTS = new Enpoints()
+export { ENDPOINTS }
 
 export const ApiEndpoint = APIEndpoint;
