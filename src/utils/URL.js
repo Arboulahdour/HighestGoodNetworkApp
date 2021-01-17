e20f283db365c521a9d4a605c4dae6d68cc39455
Nithesh AN <nithuan141@gmail.com>
2021-01-17 20:18:56 +0530

    URL correction

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,12 +1,7 @@
-/* eslint-disable import/prefer-default-export */
-import axios from 'axios'
-
 let APIEndpoint = process.env.REACT_APP_APIENDPOINT;
+
 if (!APIEndpoint) {
-  // This is to resolve the issue in azure env variable
-  // APIEndpoint = fetch('/config.json').then((data) => {
-  APIEndpoint = 'https://hgnrestdev.azurewebsites.net';
-  // });
+  APIEndpoint = 'https://hgnrestdev.azurewebsites.net/api';
 }
 
 export const ENDPOINTS = {
